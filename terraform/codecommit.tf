f980a7a4dd8866849d0eac8aa2b7400ed60c4417
Adan <adan@synthesis.co.za>
2020-04-13 12:11:33 +0200

    Removed. Created BitBucket Module

--- terraform/codecommit.tf
+++
@@ -1,3 +0,0 @@
-data "aws_codecommit_repository" "source_repository" {
-    repository_name = "${var.source_repository}" 
-}
--- terraform/s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "codebuild_artifacts" {
-    bucket = "${var.artefact_bucket_name}"
-}
