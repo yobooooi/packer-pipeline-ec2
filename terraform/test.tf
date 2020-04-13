69a441d37a055684ea9053c3a0c284e55a104b0a
Adan <adan@synthesis.co.za>
2020-04-13 12:12:41 +0200

    Updated source to BitBucket to reference Terraform Module

--- terraform/test.tf
+++ terraform/test.tf
@@ -4,12 +4,16 @@ provider "aws" {
 }
 
 module "codepipline" {
-    source = "../"
+    source = "git::https://bitbucket.org/synthesis_admin/module-aws-codepipeline.git"
 
     project_name         = "wordpress-ami-pipeline"
     project_description  = "wordpress AMI pipeline builder using the AMI builder repository"
     artefact_bucket_name = "wordpress-ami-builder-artifact-bucket"
-    packer_script        = "packer-wordpress"
     source_repository    = "ami-builder"
 
+    environment_vars = {
+      PACKER_SCRIPT  = "packer-wordpress"
+      VPC_ID         = "vpc-04f99e5833c3a909b"
+      SUBNET_ID      = "subnet-0e3c6194859275f16"
+    }
 }
