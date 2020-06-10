cf61220b38e89bd7399ed1cd56aa85a755cef4d1
Adan <adan@synthesis.co.za>
2020-06-10 11:32:28 +0200

    Example Terraform updated to extend module

--- terraform/test.tf
+++ terraform/test.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "codepipline" {
-    source = "git::https://bitbucket.org/synthesis_admin/module-aws-codepipeline.git"
+    source = "git::https://bitbucket.org/synthesis_admin/module-aws-codepipeline.git?ref=v1"
 
     project_name         = "wordpress-ami-pipeline"
     project_description  = "wordpress AMI pipeline builder using the AMI builder repository"
@@ -18,6 +18,26 @@ module "codepipline" {
     # variables are optional.
     environment_vars = {
       PACKER_SCRIPT  = "packer-wordpress"
+      VPC_ID         = "vpc-04f99e5833c3a909b"
+      SUBNET_ID      = "subnet-0e3c6194859275f16"
+    }
+}
+
+module "codepipline-squid-proxy" {
+    source = "git::https://bitbucket.org/synthesis_admin/module-aws-codepipeline.git?ref=v1.01"
+
+    project_name         = "squid-proxy-ami-pipeline"
+    project_description  = "squid AMI pipeline builder using the AMI builder repository"
+    artefact_bucket_name = "wordpress-ami-builder-artifact-bucket"
+
+    # Name of the AMI-Builder CodeCommit Repository
+    source_repository    = "ami-builder"
+
+    # Environment Variables. PACKER_SCRIPT Required, because Build-Spec references
+    # the environment variable to determine which packerfile to build from. Other
+    # variables are optional.
+    environment_vars = {
+      PACKER_SCRIPT  = "packer-squidproxy"
       VPC_ID         = "vpc-04f99e5833c3a909b"
       SUBNET_ID      = "subnet-0e3c6194859275f16"
     }
